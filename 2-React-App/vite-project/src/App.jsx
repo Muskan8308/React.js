cabeede2b140714d6c2e39beeef6ee6e779b69f0
Muskan8303 <chourasiamuskan830@gmail.com>
2025-02-01 14:21:04 +0530

    react practice

---
+++ 2-React-App/vite-project/src/App.jsx
@@ -0,0 +1,21 @@
+import KgButton from "./kgButton";
+
+function App() {
+      
+  return <div>
+    <h1>
+      Hello World
+    </h1>
+    
+     { /* Reusuable components */ }
+    <KgButton></KgButton>
+    <KgButton></KgButton>
+    <KgButton></KgButton>
+    <KgButton></KgButton>
+    <KgButton></KgButton>
+  </div>
+  
+}
+
+export default App;
+
---
+++ 2-React-App/vite-project/src/KgButton.jsx
@@ -0,0 +1,6 @@
+function KgButton()
+{
+    return <button style = {{"background-color" : 'grey', 'color' : 'white'}}>CLick here</button>
+}
+
+export default KgButton;
