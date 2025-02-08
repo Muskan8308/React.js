1934cb0f959e2fcce108e6588b41bfd2e9c17e0e
Muskan8303 <chourasiamuskan830@gmail.com>
2025-02-08 14:26:48 +0530

    state management

---
+++ 7-React-App/StateManagement/src/App.jsx
@@ -0,0 +1,11 @@
+import InputText from "./components/InputText"
+import "./App.css"
+
+
+function App() {
+    return <>
+        <InputText/>
+    </>
+}
+
+export default App;
---
+++ 7-React-App/StateManagement/src/components/InputText.jsx
@@ -0,0 +1,8 @@
+import style from "./InputText.module.css"
+
+function InputText() {
+    return <input className= {style.input} type = "text" placeholder = "Enter your Name"></input>
+    
+}
+
+export default InputText;
