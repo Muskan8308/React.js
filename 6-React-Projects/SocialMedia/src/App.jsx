eb556d989d00de51af072da73ce3dd066890bed9
Muskan8303 <chourasiamuskan830@gmail.com>
2025-02-21 11:21:14 +0530

    started a new project

---
+++ 6-React-Projects/SocialMedia/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import SideBar from "./components/SideBar";
+
+function App() {
+  return(
+    <>
+    <Header></Header>
+    <SideBar></SideBar>
+    <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
