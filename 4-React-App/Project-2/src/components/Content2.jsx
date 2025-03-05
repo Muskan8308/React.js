d06ad4ea34ff8abe4b86c3a9f63668c9c5622565
Muskan8303 <chourasiamuskan830@gmail.com>
2025-03-05 11:06:54 +0530

    updated

--- 4-React-App/Project-2/src/components/Content2.jsx
+++ 4-React-App/Project-2/src/components/Content2.jsx
@@ -1,6 +1,22 @@
+import { useEffect, useState } from "react";
+
 function Content2() 
 {
-    let time = new Date();      {/* Date() is an object of js */}
+    let [time, setTime] = useState(new Date());      {/* Date() is an object of js */}
+
+    useEffect(() => {
+        console.log("Interval started");
+        const intervalId = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+
+        // cleanup function
+
+        return () => {
+            clearInterval(intervalId);
+            console.log("Interval cancelled");
+        }
+    }, [])
 
     return <p className="lead">
         This is the current time : {time.toLocaleDateString()} - {time.toLocaleTimeString()} AM
