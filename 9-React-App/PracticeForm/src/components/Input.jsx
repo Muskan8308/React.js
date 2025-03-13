d91e8c1847862f9cc7a3206c119ea3709151fddc
Muskan8303 <chourasiamuskan830@gmail.com>
2025-03-13 19:20:37 +0530

    learned how to submit data with fetch

---
+++ 9-React-App/PracticeForm/src/components/Input.jsx
@@ -0,0 +1,111 @@
+import { useState } from "react";
+
+function Input() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      fetch("https://dummyjson.com/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          username: name,
+          password: password,
+          expiresInMins: 30, 
+        }),
+        credentials: "include", 
+      })
+        .then((res) => res.json())
+        .then(console.log);
+
+      // if (!res.ok) {
+      //   throw new Error("failed to submit data");
+      // }
+
+      alert("Data submitted successfully!!");
+
+    } catch (error) {
+      console.error("Error : ", error);
+      alert("Failed to submit data");
+    }
+  };
+
+  return (
+    <>
+      <center>
+        <form onSubmit={handleSubmit}>
+          <label>
+            <div className="mb-3">
+              <label htmlFor="exampleInputName1" className="form-label">
+                Name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="exampleInputName1"
+                name="name"
+                aria-describedby="emailHelp"
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="exampleInputEmail1" className="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                name="email"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                onChange={handleChange}
+                required
+              />
+              <div id="emailHelp" className="form-text">
+                We'll never share your email with anyone else.
+              </div>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="exampleInputPassword1" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                className="form-control"
+                id="exampleInputPassword1"
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="mb-3 form-check">
+              <input
+                type="checkbox"
+                className="form-check-input"
+                id="exampleCheck1"
+              />
+              <label className="form-check-label" htmlFor="exampleCheck1">
+                Check me out
+              </label>
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </label>
+        </form>
+      </center>
+    </>
+  );
+}
+export default Input;
